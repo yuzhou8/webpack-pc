3cc291e16265e627fdf3e576fa3a7d64c7ec5a45
邓小宇 <625071541@qq.com>
2020-07-15 17:47:00 +0800

    2020-7-15

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import compositionApi from '@vue/composition-api'
+import Plugin from 'yu-vue'
 import App from './app.vue'
 import router from './router/router.js'
 import './public/index.css'
 
 Vue.use(compositionApi)
+Vue.use(new Plugin('//at.alicdn.com/t/font_1139100_rlq2wt5r6z.js'))
 
 /* eslint-disable no-new */
 new Vue({
